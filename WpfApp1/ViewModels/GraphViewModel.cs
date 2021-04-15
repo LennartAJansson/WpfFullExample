8846ac399a3b7d97c5bb8ed55b9007afbacc4428
Lennart Jansson <lennart.jansson@sigma.se>
2021-04-15 15:31:22 +0200

    Binding of Loaded in WpfPlot component in GraphView to ViewModel to get the WpfPlot object

--- WpfApp1/ViewModels/GraphViewModel.cs
+++ WpfApp1/ViewModels/GraphViewModel.cs
@@ -1,8 +1,28 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+using ScottPlot;
+
+using System.Threading.Tasks;
 
 namespace WpfApp1.ViewModels
 {
     public class GraphViewModel : ObservableObject
     {
+        private WpfPlot wpfPlot;
+
+        public AsyncRelayCommand<WpfPlot> LoadedCommand { get; }
+
+        public GraphViewModel()
+        {
+            LoadedCommand = new AsyncRelayCommand<WpfPlot>(LoadedWpfPlot);
+        }
+
+        private Task LoadedWpfPlot(WpfPlot wpfPlot)
+        {
+            this.wpfPlot = wpfPlot;
+
+            return Task.CompletedTask;
+        }
     }
 }
